936fc99c54edd6eacee8ce8d5a2c3fc9a1476e7a
Gaurav <gameider12@gmail.com>
2019-04-08 19:43:38 -0400

    documentation of genReads function

---
+++ R/genReads.R
@@ -0,0 +1,10 @@
+#' A function to generate reads with given error and save in fastq format
+#' @param readLen length of reads required
+#' @param error percentage error in reads
+#' @param sequence sequence to generate reads from
+#' @param coverage sequencing coverage desired
+#' 
+
+genReads <- function(sequence, readLen = 10000, error = 0.10, coverage = 30){
+	
+}
