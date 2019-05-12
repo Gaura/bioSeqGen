05ac1e36a824cc9808ed2685864b432c0e503688
Gaurav <gameider12@gmail.com>
2019-05-12 11:18:23 -0400

    added print statements for clarity

--- R/testGraph10k.R
+++ R/testGraph10k.R
@@ -1,17 +1,17 @@
 library(seqinr)
 source('/work-zfs/mschatz1/genomescopeLR/bioSeqGen/R/functions.R')
 read.fasta('/work-zfs/mschatz1/genomescopeLR/bioSeqGen/data/ecoliGenome.fasta') -> ecoliSeq
+print('Loading Genome')
 paste0(ecoliSeq$NC_011750.1,collapse = '') -> ecoliGenome
 ecoliGenome <- ecoliGenome[1]
 ecoli10k <- substr(ecoliGenome,1,1e4)
 ecoli10k <- toupper(ecoli10k)
 reads10k <- generateReads( genome = ecoli10k, readLen = 1000)
 reads10kWith5PcentError <- flipReads(reads10k,0.05)
-print('hi')
+print('Getting reads and kmers')
 kmers10kWith5pcentError <- sapply(1:length(reads10k),function(x) getKmersFromRead(reads10kWith5PcentError[x]))
 kmers10k <- sapply(1:length(reads10k),function(x) getKmersFromRead(reads10k[x]))
 kmers10k <- unlist(kmers10k)
-beepr::beep(8)
 kmers10kWith5pcentError <- unlist(kmers10kWith5pcentError) #Unlist kmers
 #get Table
 kmers10kE05X30T <- table(kmers10kWith5pcentError) 
@@ -20,6 +20,7 @@ kmers10kE05X30Df <- as.data.frame(kmers10kE05X30T, stringAsFactors = FALSE )
 #remove Table
 rm(kmers10kE05X30T)
 library(stringdist)
+print('Calculating Distance')
 refKmer <- paste0(rep('A',21),collapse = '')
 refDist10kE05X30 <- sapply(1:nrow(kmers10kE05X30Df),function(x){stringdist(kmers10kE05X30Df[x,1],refKmer,method = "hamming")})
 kmers10kE05X30Df$refDist <- refDist10kE05X30
@@ -28,7 +29,7 @@ kmers10kE05X30Df <- kmers10kE05X30Df[-which(kmers10kE05X30Df$refDist == Inf),]
 kmers10kE05X30Df <- kmers10kE05X30Df %>% arrange(refDist)
 kmers10kE05X30Df$index <- seq(1,nrow(kmers10kE05X30Df))
 kmers10kE05X30Df$connected <- rep('0',nrow(kmers10kE05X30Df))
-
+print('Finding connected kmers')
 dists <- sort(unique(kmers10kE05X30Df$refDist))
 sapply(1:(length(dists)-1), function(x){
       km10kE05X30D67 <- kmers10kE05X30Df %>% filter(refDist == dists[x] | refDist == dists[x+1])
